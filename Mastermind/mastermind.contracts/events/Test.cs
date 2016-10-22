22a1f12d554c8eea569d3d0f316701b30812d6a8
Thomas Leidl <toom-l@hotmail.de>
2016-10-22 17:14:57 +0200

    Test

---
+++ Mastermind/mastermind.contracts/events/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mastermind.contracts.events
+{
+    class Test
+    {
+    }
+}
